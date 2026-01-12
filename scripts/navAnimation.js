a874a2cbffb55d761388a2d210393ebaf7a38bd9
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2026-01-12 13:23:25 -0300

    Button download and scroll animation was added

--- scripts/navAnimation.js
+++ scripts/navAnimation.js
@@ -1,17 +1,41 @@
   /*Navigation header animations*/ 
   document.addEventListener('DOMContentLoaded', () => {
-    // Selecciona todos los enlaces del header
     const navLinks = document.querySelectorAll('header nav a');
-  
+    
     navLinks.forEach(link => {
       link.addEventListener('click', () => {
         navLinks.forEach(l => {
-          //l.classList.remove('border-b', 'border-b-white');
           l.classList.remove('underline', 'underline-4px');
         });
-        //link.classList.remove('text-primary');
-        //link.classList.add('border-b', 'border-b-white');
         link.classList.add('underline', 'underline-4px');
       });
     });
-  });
+    // Navigation header animations intersection observer
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver((entries) => {
+      const visibleSections = entries.filter(entry => entry.isIntersecting);
+      if (!visibleSections.length) return;
+
+      const mostVisible = visibleSections.reduce((prev, current) => {
+        return current.intersectionRatio > prev.intersectionRatio
+          ? current
+          : prev;
+      });
+      const id = mostVisible.target.id;
+      navLinks.forEach(link => {
+        link.classList.remove('underline', 'underline-4px');
+      });
+      const activeLink = document.querySelector(`header nav a[href="#${id}"]`);
+      if (activeLink) {
+        activeLink.classList.add('underline', 'underline-4px');
+      }
+    }, {
+      threshold: 0.3
+    });
+    sections.forEach(section => observer.observe(section));
+  });      
+    
+
+
+  
